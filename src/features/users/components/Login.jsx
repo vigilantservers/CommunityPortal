1d22f6febd9940b433baaf701909f3526e1ac8a5
Tom Hewison <tomhewison@Toms-MacBook-Pro.local>
2023-01-17 16:33:27 +0000

    17/01/23

--- src/features/users/components/Login.jsx
+++ src/features/users/components/Login.jsx
@@ -1,22 +1,65 @@
-import React from "react";
 import "./Login.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Login = () => {
+import config from '../../../config';
+
+const client = axios.create({
+  baseURL: config.apiBaseURL,
+});
+
+const Login = (props) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    // check if the user is already logged in
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken) {
+      // if yes, redirect to the home page
+      props.history.push('/');
+    }
+  }, []);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post('/api/Auth/login', { email, password });
+      const { accessToken, refreshToken, user } = response.data.data;
+
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('refreshToken', refreshToken);
+      localStorage.setItem('user', JSON.stringify(user));
+
+      // redirect to the home page
+      props.history.push('/');
+    } catch (err) {
+      setError(err.response.data.message);
+    }
+  };
   return (
     <div className="login-container">
       <div className="login-header">
         <h1>Login</h1>
       </div>
       <div className="login-content">
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" />
-          <br />
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" />
-          <br />
-          <button type="submit">Log in</button>
-        </form>
+      <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      {error && <p>{error}</p>}
+      <button type="submit">Login</button>
+    </form>
         <div className="login-footer">
           <div className="register">
             <p>
